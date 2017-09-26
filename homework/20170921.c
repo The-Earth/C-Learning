dfef0032beaa9476be4372bc7f03aae947d42e32
The-Earth <tigerzeng97@gmail.com>
2017-09-26 15:46:31 +0800

    function 1

---
+++ homework/20170921.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+int function1()
+{
+	int a, b;
+	double res;
+	printf("a = ?\n");
+	scanf("%d",&a);
+	printf("b = ?\n");
+	scanf("%d,&b");
+	
+	res = sqrt((a + b + abs(a - b)) / 2);
+	
+	printf("The result is %.3f",res);
+	
+	return 0;
+}
+
+int main()
+{
+	function1();
+}

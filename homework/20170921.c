154b3b03ead4be20fa8c0d136824c7137a583b74
The-Earth <tigerzeng97@gmail.com>
2017-09-26 18:57:18 +0800

    function2

--- homework/20170921.c
+++ homework/20170921.c
@@ -12,7 +12,20 @@ int function1()
 	
 	res = sqrt((a + b + abs(a - b)) / 2);
 	
-	printf("The result is %.3f",res);
+	printf("The result is %.3f\n\n",res);
+	
+	return 0;
+}
+
+int function2()
+{
+	double num, squ, pw;
+	
+	printf("Input a real number:");
+	scanf("%f",&num);
+	squ = sqrt(num);
+	pw = pow(num,2);
+	printf("Square root of the input is %.3f\nSquare of the input is %.3f",squ,pw);
 	
 	return 0;
 }
@@ -20,6 +33,7 @@ int function1()
 int main()
 {
 	function1();
+	function2();
 	
 	return 0;
 }

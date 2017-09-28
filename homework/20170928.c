cb6b7eaecca79eb6865459022aaaf4bef9e3f806
The-Earth <tigerzeng97@gmail.com>
2017-09-28 15:41:50 +0800

    0928 Homework task 1
    triangle area

---
+++ homework/20170928.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int triangle()
+{
+	double a, b, c, area, s; 
+	printf("Calculating the area of a given triangle.\nInput length of the three sides (seperated by space):");
+	scanf("%lf %lf %lf", &a, &b, &c);
+	if (a + b > c && a + c > b && b + c > a)
+	{
+		s = (a + b + c) / 2;
+		area = sqrt(s * (s - a) * (s - b) * (s - c));
+		printf("\nThe area of the given triangle is %.2f",area);
+	}
+	else
+	{
+		printf("It is not a triangle.\n");
+	}
+	
+	return 0;
+}
+
+int main()
+{
+	printf("task1:\n");
+	triangle();
+	
+	return 0;
+}

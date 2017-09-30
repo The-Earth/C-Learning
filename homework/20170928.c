e87fd713991cf84a15bd3b040f3b828526e032bf
The-Earth <tigerzeng97@gmail.com>
2017-09-30 15:33:43 +0800

    task 6
    Expressions

--- homework/20170928.c
+++ homework/20170928.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
- 
+
 int triangle()
 {
 	double a = 0, b = 0, c = 0, area, s; 
@@ -82,14 +82,29 @@ int tenK()
 	t = (num % 100) / 10;
 	o = num % 10;
 	
-	printf("%d(%d+%d+%d+%d+%d)",(tk + k + h + t + o), tk, k, h, t, o);
+	printf("%d(%d+%d+%d+%d+%d)\n\n",(tk + k + h + t + o), tk, k, h, t, o);
+	
+	return 0;
+}
+
+int error()
+{
+	int x=1,y=2,z=3,u=4,c=5,r=6,a=7,b=8,e=9,R=10;
+	const double Pi=3.14159;
+	
+	printf("|(x+y)(z+u)+2c|=%f\nPi r^2/(a+b)=%f\n(lnx+cosy)/3=%f\n4x^3+2e^y=%f\n4Pi R^2=%f\n\n",
+	fabs((x+y)*(z+u)+2*c),
+	Pi*pow(r,2)/(a+b),
+	(log(x)+cos(y))/3,
+	4*pow(x,3)+2*pow(e,y),
+	4*Pi*pow(R,2));
 	
 	return 0;
 }
 
 int main()
 {
-	printf("Task 1:\n");
+	/*printf("Task 1:\n");
 	triangle();
 	printf("Task 2:\n");
 	ASCII();
@@ -98,7 +113,10 @@ int main()
 	printf("Task 4:\n");
 	d2hob();
 	printf("Task 5:\n");
-	tenK();
+	tenK();*/
+	printf("Task 6:\n");
+	error();
+	
 		
 	return 0;
 }

cacf40c839cd040a3af5cb6fb6824bac823c75b7
The-Earth <tigerzeng97@gmail.com>
2017-10-12 15:30:12 +0800

    Task 1 done

--- homework/20171012.c
+++ homework/20171012.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void lower()
+{
+	printf("Give me an English character and I will tell you something:");
+	char lec;
+	scanf("%c",&lec);
+	
+	if (lec >= 'A' && lec <= 'Z')
+	{
+		printf("The result is \"%c\"\n\n",lec+32);
+	}
+	else if (lec >= 'a' && lec <= 'z')
+	{
+		printf("The result is \"%c\"\n\n",lec);
+	}
+	else
+	{
+		printf("What are you doing??\n\n");
+	}
+}
 
+int main()
+{
+	printf("This is the homework for Oct. 12 lecture.\n\n");
+	printf("Task 1:\n\n");
+	lower();
+	
+	return 0;
+}

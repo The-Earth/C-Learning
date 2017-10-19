0dbbab91e6b0c1cb8fa4f6f60b9685ddc7469940
The-Earth <tigerzeng97@gmail.com>
2017-10-19 15:16:31 +0800

    Task 1 done.

---
+++ homework/20171019.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void A2Z()
+{
+	char ser; int i;
+	for(ser=65; ser < 91; ++ser)
+	{
+		printf("%c\t",ser);
+		if((int) ser % 8 == 0)
+		{
+			printf("\n");
+		}
+	}
+}
+
+int main()
+{
+	printf("Homework for 2017/10/19 lecture.\n\n");
+	printf("Task 1:\n\n");
+	A2Z();
+	
+	return 0;
+}

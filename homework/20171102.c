9ae2b65bbf9804fc0de901471f950c16466ab4b6
The-Earth <tigerzeng97@gmail.com>
2017-11-02 16:14:13 +0800

    sin done

--- homework/20171102.c
+++ homework/20171102.c
@@ -9,7 +9,7 @@ double randf(double min, double max)
 	return (r / RAND_MAX * (max - min) + min);
 }
 
-double PI(int n)
+double PI(int n) //n越大，计算精度越高 
 {
 	int i, in = 0; double x , y;
 	for(i = 1 ; i <= n ; ++i)
@@ -21,14 +21,40 @@ double PI(int n)
 	return (in / (double) n * 4);
 }
 
+double SIN(int n , double pi)
+{
+	int i, in = 0; double x , y;
+	for(i = 1 ; i <= n ; ++i)
+	{
+		x = randf(0 , pi / 2) ; y = randf(0 , 1);
+		if(y <= sin(x))
+			in += 1;
+	}
+	return (in / (double) n * pi / 2);
+}
+
 int main()
 {
 	srand((unsigned)time(NULL));
 	//Pi start
+	double pi;
 	puts("--------Calculating Pi......--------");
-	printf("%.10f",PI(100000000));
+	pi = PI(1000000000);
+	printf("%.10f",pi);
 	puts("\n--------Calculation ended.--------\n");
 	//Pi end
 	
 	system("pause");
+	
+	//Sin() start
+	puts("--------Integrating Sin(x) from 0 to pi/2");
+	printf("%.10f",SIN(10000000,3.1415926));
+	puts("\n--------Integration ended.--------\n");
+	//Sin() end
+	
+	system("pause");
+	
+	
+	
+	return 0;
 }

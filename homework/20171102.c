70cec9db12f8de133ae8d4d3670864a6dcf7ea07
The-Earth <tigerzeng97@gmail.com>
2017-11-02 16:00:23 +0800

    pi done

--- homework/20171102.c
+++ homework/20171102.c
@@ -11,15 +11,24 @@ double randf(double min, double max)
 
 double PI(int n)
 {
-	int i ; double x , y;
-	for(i = 0 ; i <= n ; ++i)
+	int i, in = 0; double x , y;
+	for(i = 1 ; i <= n ; ++i)
 	{
 		x = randf(-1 , 1) ; y = randf(-1 , 1);
-		
+		if(pow(x,2) + pow(y,2) < 1)
+			in += 1;
 	}
+	return (in / (double) n * 4);
 }
 
 int main()
 {
-
+	srand((unsigned)time(NULL));
+	//Pi start
+	puts("--------Calculating Pi......--------");
+	printf("%.10f",PI(100000000));
+	puts("\n--------Calculation ended.--------\n");
+	//Pi end
+	
+	system("pause");
 }

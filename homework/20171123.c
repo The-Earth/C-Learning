480700dc2c16792f7759701fae31cc4f149da286
The-Earth <tigerzeng97@gmail.com>
2017-11-23 15:50:25 +0800

    Task 1 done

---
+++ homework/20171123.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void select_sort()
+{
+	puts("Input an array with length 10 as guided.");
+	int i , a[10];
+	for(i = 0 ; i < 10 ; ++i)
+	{
+		printf("Input element with index %d : ", i);
+		scanf("%d" , &a[i]);
+	}
+	
+	for(i = 0 ; i < 10 ; ++i)
+	{
+		int j , tmp , max_index;
+		max_index = i;
+		for(j = i ; j < 10 ; ++j)
+			max_index = a[j] > a[max_index] ? j : max_index ; 
+		
+		tmp = a[i] ; a[i] = a[max_index] ; a[max_index] = tmp;
+	}
+	
+	puts("Ranked in decending sort:");	//Ωµ–Ú≈≈¡– 
+	for(i = 0 ; i < 10 ; ++i)
+		printf("%d\t", a[i]);
+}
+
+int main()
+{
+	//—°‘Ò≈≈–Ú 
+	select_sort();
+	system("pause");
+	
+	return 0;
+}

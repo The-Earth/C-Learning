5c37b86c9a94d2b7bbfd28a599a23f96ab2091ff
The-Earth <tigerzeng97@gmail.com>
2017-12-02 22:52:26 +0800

    Task 3 done

--- homework/20171130.c
+++ homework/20171130.c
@@ -7,6 +7,10 @@
 void word_sort();
 struct student;
 void mark_sort();
+void div_find();
+int randf(double min , double max);
+int div_find_core(int arr[] , int tar , int len);
+struct array;
 
 void word_sort()	//排列单词 
 {
@@ -66,14 +70,87 @@ void mark_sort()
 		printf("%s\t%.1f\n", stulist[i].name , stulist[i].mark);
 }
 
+struct array
+{
+	int index;
+	int value;
+};
+
+void div_find()	//二分法查找 
+{ 
+	struct array arr[1000] , tmp;
+	puts("生成的随机数组为：\n序号\t数值");
+	system("pause");
+	for(int i = 0 ; i < 1000 ; ++i)
+	{
+		arr[i].value = randf(0 , 999);
+		arr[i].index = i;
+		printf("%d\t%d\n" , i , arr[i].value);
+	}
+	
+	for(int i = 0 ; i < 1000 ; ++i)	//排序 
+	{
+		for(int j = 999 ; j >= i ; --j)
+		{
+			if(arr[j].value < arr[j - 1].value)
+			{
+				tmp = arr[j] ; arr[j] = arr[j-1] ; arr[j-1] = tmp ;
+			}
+		}
+	}
+	
+	int target , arrval[1000];
+	puts("输入需要查找的数");
+	scanf("%d", &target);
+	
+	for(int i = 0 ; i < 1000 ; ++i)
+	{
+		arrval[i] = arr[i].value;
+	}
+	
+	int res = div_find_core(arrval , target , 1000) ;
+	if(res >= 0)
+		printf("一个目标数的索引为%d", arr[res].index);
+	else
+		printf("未查找到目标。");
+}
+
+int div_find_core(int arr[] , int tar , int len)
+{
+	int max = len - 1 , min = 0;
+	int mid ;
+	while(max >= min)
+	{
+		mid = (max + min) / 2;
+		if(arr[mid] == tar)
+			return mid;
+		if(arr[mid] > tar)
+			max = mid - 1;
+		else if(arr[mid] < tar)
+			min = mid + 1;
+	}
+	return -1;
+}
+
+int randf(double min, double max)	//随机数生成器 
+{
+	double r;
+	r = rand();
+	return (int)(r / RAND_MAX * (max - min) + min);
+}
+
 int main()
 {
+	srand((unsigned)time(NULL));
 	/*
 	//单词冒泡 
 	word_sort();
-	system("pause");*/
+	system("pause");
 	
 	mark_sort();
+	system("pause");*/
+	
+	div_find();
 	
 	return 0;
 }

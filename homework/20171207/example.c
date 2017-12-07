a6ab5585a2d95b516e0900cf059df7b2e31ab827
The-Earth <tigerzeng97@gmail.com>
2017-12-07 15:33:17 +0800

    10.1.3

--- homework/20171207/example.c
+++ homework/20171207/example.c
@@ -12,9 +12,27 @@ void _10_1_1()
 	printf("b的地址（十六进制）：\t%X\n",&b[1]);
 }
 
+void _10_1_3()
+{
+	int i , a[] = {1,3,5,7,9} , *p = a;
+	for(i = 0 ; i < 5 ; i++)
+		printf("a[%d] = %d\t" , i , a[i]);
+	printf("\n");
+	printf("a = %d\n", p);				//输出数组a 的首地址
+	printf("p + 2 = %d\n" , p+2);		//输出 a[2]的地址
+	printf("*p+3 = %d\n" , *p + 3);		//输出a[0]+3的值
+	printf("*(p+3) = %d\n" , *(p+3));	//输出a[3]的值
+	printf("*p++ = %d\n" , *p++);		//输出a[0]的值后，使p指向a[1]
+	p = a;
+	printf("*++p = %d\n" , *++p);		//使p指向a[2]后，输出a[2]的值
+	printf("++*p = %d\n" , ++*p);		//输出++a[2]的值 
+}
+
 int main()
 {
 	_10_1_1();
+	puts("\n");
+	_10_1_3();
 	
 	return 0;
 }

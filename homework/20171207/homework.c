7036ee9c9c58862badc0112efad44210c29c052a
The-Earth <tigerzeng97@gmail.com>
2017-12-12 21:14:00 +0800

    P205 5 done

--- homework/20171207/homework.c
+++ homework/20171207/homework.c
@@ -16,6 +16,16 @@ void str_cpy(char src[] , char *p)
 		*(p+i)= src[i++];
 }
 
+double arr_average(double *arr , int len)
+{
+	double sum = 0;
+	for(int i = 0 ; i < len ; ++i)
+	{
+		sum += *(arr + i);
+	}
+	return sum / len;
+}
+
 int main()
 {
 	//P205 2
@@ -24,12 +34,22 @@ int main()
 	Circle(Rad, &C , &Area);
 	printf("\n%f\n%f" , C , Area);
 	puts("\n========");
+	system("pause");
 	
 	//P205 4
 	char src[32]="abcdefg" , dest[32];
 	char *p = dest;
 	str_cpy(src , p);
 	printf("%s" , dest);
+	puts("\n========");
+	system("pause");
+	
+	//P205 5
+	double srcarr[5] = {2,4,3,2,1};
+	printf("%f",arr_average(srcarr , 5));
+	puts("\n========");
+	system("pause");
+	
 	
 	return 0;
 }

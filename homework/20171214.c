012e16c9cdd1b67f5ffed177a79032e05078bc36
The-Earth <tigerzeng97@gmail.com>
2017-12-14 16:34:58 +0800

    P207 7 done

--- homework/20171214.c
+++ homework/20171214.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void string_del(char *obj , int k , int m , int len)
+void del_pos(char *obj , int k , int m , int len)
 {
 	
 	for(int i = k - 1 ; i < k + m - 1 ; ++i)
-	{
 		*(obj + i) = *(obj + i + m);
-	}
 	
 	if(k + m > len)
 		*(obj + k) = '\0';
@@ -16,10 +14,39 @@ void string_del(char *obj , int k , int m , int len)
 		*(obj + len - m) = '\0';
 }
 
+void move_1(char *obj , int k)
+{
+	int i = 0;
+	do
+		*(obj + k + i) = *(obj + k + i + 1);
+	while(*(obj + k + i++));
+		
+}
+
+void del_par(char *obj , char let)
+{
+	int i = -1;
+	do
+	{
+		if(*(obj + ++i) == let)
+			move_1(obj , i);
+	}while(*(obj + i));
+}
+
 int main()
 {
+	//P207 6
 	char s[] = "abcde" , *p = s ;
-	string_del(p , 4 , 1 , sizeof(s)-1);
+	del_pos(p , 4 , 1 , sizeof(s)-1);
 	printf("%s",s);
+	puts("\n");
+	
+	//P207 7
+	char t[] = "teacher" , let='e';
+	p = t;
+	del_par(p , let);
+	printf("%s",t);
+	
+	
 	return 0;
 }

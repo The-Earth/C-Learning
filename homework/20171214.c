cf373b33a75940c151423827ac6121651bbe5cea
The-Earth <tigerzeng97@gmail.com>
2017-12-14 16:01:59 +0800

    P207 6

--- homework/20171214.c
+++ homework/20171214.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
+void string_del(char *obj , int k , int m , int len)
+{
+	
+	for(int i = k - 1 ; i < k + m - 1 ; ++i)
+	{
+		*(obj + i) = *(obj + i + m);
+	}
+	
+	if(k + m > len)
+		*(obj + k) = '\0';
+	else
+		*(obj + len - m) = '\0';
+}
 
 int main()
 {
-
+	char s[] = "abcde" , *p = s ;
+	string_del(p , 4 , 1 , sizeof(s)-1);
+	printf("%s",s);
 	return 0;
 }

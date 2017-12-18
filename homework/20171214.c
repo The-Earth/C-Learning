c1a788b18ce0d0952102ccf7840f4d71d4c83330
The-Earth <tigerzeng97@gmail.com>
2017-12-18 21:43:03 +0800

    comment

--- homework/20171214.c
+++ homework/20171214.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+//########################################
+//#                                      #
+//#                                      #
+//#   由于本源代码文件基于作业要求编写   #
+//#        没有进行用户体验优化          #
+//#          不建议编译后运行            #
+//#                                      #
+//########################################
+
 void del_pos(char *obj , int k , int m , int len)
 {
 	
@@ -39,10 +48,10 @@ typedef struct
 	int age;
 } student;
 
-int age_sort_input(student *data, int i)
+int age_sort_input(student *data, int i , int n)
 {
 	int age_tmp;
-	printf("输入第%d个学生的姓名和年龄（姓名只接受ASCII，以空格分隔），姓名输入end以终止输入，最多输入40组：" , i);
+	printf("输入第%d个学生的姓名和年龄（姓名只接受ASCII，以空格分隔），姓名输入end以终止输入，最多输入%d组：" , i , n);
 	scanf("%s %d", &((*data).name) , &((*data).age));
 	if(strcmp((*data).name , "end") == 0)
 		return -1;
@@ -73,26 +82,33 @@ void age_sort_output(student *p , int len)
 
 void age_sort_main()
 {
-	int i = 1;
-	student data[39];
+	int i = 1 , n;
+	puts("输入学生个数：");
+	scanf("%d" , &n);
+	student data[n];
 	do
 	{
-		if(age_sort_input(&data[i-1] , i) == -1)
+		if(age_sort_input(&data[i-1] , i , n) == -1)
 			break;
-	} while(++i < 40);
+	} while(++i <= n);
 	
 	int len = i - 1;
 	
 	age_sort_core(data , len);
-	
+	puts("降序排列如下：");
 	age_sort_output(data , len);
 }
 
 int main()
 {
+	puts("########################################\n#                                      #\n#                                      #\n#   由于本源代码文件基于作业要求编写   #\n#        没有进行用户体验优化          #\n#          不建议编译后运行            #\n#                                      #\n########################################");
 	//P207 6
+	puts("P207 6");
+	puts("abcde 从第？个字符起删除？个字符：");
+	int m , n;
 	char s[] = "abcde" , *p = s ;
-	del_pos(p , 4 , 1 , sizeof(s)-1);
+	scanf("%d %d" , &m ,&n);
+	del_pos(p , m , n , sizeof(s)-1);
 	printf("%s",s);
 	puts("\n");
 	

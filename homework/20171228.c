17d2d82ac6eddf3b90b31e09b05cdea9a7a2d179
The-Earth <tigerzeng97@gmail.com>
2017-12-31 15:58:17 +0800

    Student info / Not done

---
+++ homework/20171228.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define STUDENT struct student
+
+struct student
+{
+	char name[32];
+	int mark;
+	STUDENT *next;
+};
+
+STUDENT *init()
+{
+	STUDENT *head = (STUDENT*)malloc(sizeof(STUDENT));
+	puts("输入第一个学生的姓名成绩：");
+	scanf("%s %d", &(head->name), &(head->mark));
+	head->next = NULL;
+	return head;
+}
+
+STUDENT *input_core(STUDENT *pre)
+{
+	STUDENT *cur = (STUDENT *)malloc(sizeof(STUDENT));
+	char name_tmp[32]; int mark_tmp;
+	
+	puts("输入姓名 成绩：");
+	scanf("%s %d", &name_tmp, &mark_tmp);
+	if(mark_tmp == -1)
+		return NULL;
+	strcpy(cur->name, name_tmp);
+	cur->mark = mark_tmp;
+	cur->next = NULL;
+	pre->next = cur;
+	
+	return cur;
+}
+
+void input(STUDENT *p)
+{
+	while(1)	//input
+	{
+		STUDENT *pre = input_core(p);
+		if(pre == NULL)
+			break;
+		else
+			p = pre;
+	}
+}
+
+void show(STUDENT *p)
+{
+	printf("%s\t%d\n", p->name, p->mark);
+	if(p->next)
+		show(p->next);
+}
+
+int search_name(STUDENT *head, char key[32])
+{ 
+	if(strcmp(key, head->name) == 0)
+		{
+		printf("%s的成绩为：%d", head->name, head->mark);
+		return 1;
+		}
+	else
+	{
+		if(!(head->next))
+		{
+			puts("未找到");
+			return 0;
+		}
+		else
+			search_name(head->next, key);
+	}
+}
+
+int search_mark(STUDENT *head, int key)
+{
+	printf("成绩为%d的学生有：", key);
+	if(key == head->mark)
+		printf("%s", head->name);
+	else
+	{
+		if(head->next == NULL)
+		{
+			puts("未找到");
+			return 0;
+		}
+		else
+			search_mark(head->next, key);
+	}
+}
+
+void free_memory(STUDENT *p)
+{
+	if(p->next == NULL)
+		free(p);
+	else
+		free_memory(p->next);
+}
+
+void stu_info()
+{
+	func:puts("选择：\n1: 初始化链表\n2：输入数据\n3：显示所有数据\n4：按姓名查找\n5：按成绩查找\n6：删除指定学生\n7：退出");
+	int opt , exit = 0;
+	char namekey[32];
+	int markkey;
+	
+	STUDENT *head = NULL;
+	STUDENT *p = NULL;
+	scanf("%d", &opt);
+	switch(opt)
+	{
+		case 1:
+			head = init();
+			p = head;
+			break;
+		case 2:
+			input(p);
+			p = head;
+			break;
+		case 3:
+			show(p);
+			p = head;
+			break;
+		case 4:
+			puts("输入要查找的姓名："); 
+			scanf("%s", namekey);
+			search_name(p , namekey);
+			p = head;
+			break;
+		case 5:
+			
+			puts("输入要查找的分数："); 
+			scanf("%d", &markkey);
+			search_mark(p , markkey);
+			p = head;
+			break;
+		case 6:
+			
+		case 7:
+			exit = 1;
+	}
+	if(exit == 0)
+		goto func;
+	free_memory(head);
+}
+
+int main()
+{
+	stu_info();
+	
+	return 0;
+}
